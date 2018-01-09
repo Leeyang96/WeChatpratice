bbb5d63fc81c17f85796891d9c67beee81402c0a
leeyang <577724744@qq.com>
2018-01-09 17:44:13 +0800

    switch

---
+++ switch/pages/index/index.js
@@ -0,0 +1,12 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  switch1Change: function (e) {
+    console.log('switch1 发生 change 事件，携带值为', e.detail.value)
+  },
+  switch2Change: function (e) {
+    console.log('switch2 发生 change 事件，携带值为', e.detail.value)
+  }
+})
