505c5c4fff1ab0d28e514f5085cf514085b66c33
leeyang <577724744@qq.com>
2018-01-17 23:09:35 +0800

    live_player

---
+++ live_player/pages/index/index.js
@@ -0,0 +1,12 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  statechange(e) {
+    console.log('live-player code:', e.detail.code)
+  },
+  error(e) {
+    console.error('live-player error:', e.detail.errMsg)
+  }
+})
